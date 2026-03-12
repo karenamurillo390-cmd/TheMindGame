7b827e4271173b7654e243684a25c34d99f72511
karenamurillo390-cmd <karenamurillo390@gmail.com>
2026-03-12 05:51:05 -0500

    Add Jugador and Partida classes in LaMente.cpp

--- LaMente.cpp
+++ LaMente.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Jugador{
+private:
+    string nombre;
+    int cantCartas;
+
+public:
+    Jugador(string nombre, int cartas){
+        this->nombre = nombre;
+        cantCartas = cartas;
+    }
+
+    void lanzarCarta(){
+        cout << nombre << " lanza una carta" << endl;
+    }
+
+    void recibirCarta(){
+        cantCartas++;
+    }
+};
+
+class Partida{
+private:
+    int nivel;
+    int cartasEnJuego;
+    int jugadores;
+    int vidas;
+    int shuriken;
+
+public:
+    Partida(int n,int c,int j,int v,int s){
+        nivel = n;
+        cartasEnJuego = c;
+        jugadores = j;
+        vidas = v;
+        shuriken = s;
+    }
+
+    void repartirCartas(){
+        cout<<"Repartiendo cartas..."<<endl;
+    }
+};
+
+int main(){
+
+    Jugador jugador1("Ana",3);
+
+    jugador1.lanzarCarta();
+
+    return 0;
+}
